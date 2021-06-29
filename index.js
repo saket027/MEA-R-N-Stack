2eff0ac9a829e8a98cad1e08b0ea96a679c2bad3
Saket M <73999911+saket027@users.noreply.github.com>
2021-06-29 07:01:30 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-
-app.listen("3001", () => {
-  logger.info(`Server running on port 3001`)
-})
